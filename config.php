eb60f94375a10739932b32d302c4151a5a7d07b9
Praharsh1029 <113592326+Praharsh1029@users.noreply.github.com>
2023-12-07 01:44:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'praharshsql.mysql.database.azure.com');
+define('DB_USERNAME', 'praharsh');
+define('DB_PASSWORD', 'AlphaBetaGama%9');
 define('DB_NAME', 'quiz');
-$host = "localhost";
+$host = "praharshsql.mysql.database.azure.com";
 $dbname = "quiz";
-$username = "root";
-$password = "";
+$username = "praharsh";
+$password = "AlphaBetaGama%9";
 /* Attempt to connect to MySQL database */
 $link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
